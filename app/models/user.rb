aba3d6344a7ff4eac5faa39d543a5a2c77ddd08f
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-04-19 20:36:38 -0700

    correct relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_secured_password
+
   has_many :mission
+  has_many :task, through: :mission
 end
