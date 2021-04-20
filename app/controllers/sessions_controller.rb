f855ba5b587923ce16a91c84db68d683c4f79a85
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-04-20 11:31:12 -0700

    add methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,54 @@
 class SessionsController < ApplicationController
 
 
-  get '/login' do 
-      if logged_in?
-        user_id = session[:user_id]
+  def new
+    if logged_in
+      redirect_to mission_path
+      flash[:notice] = "You are already logged in."
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      log_in(user)
+      flash[:success] = "Logged in!"
+      redirect_to mission_path
+    else
+      flash[:error] = "Something Wrong Happened, Please Try Again."
+      redirect_to login_path
+    end
+  end
+
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+  # get '/login' do 
+  #     if logged_in?
+  #       user_id = session[:user_id]
         
-        redirect to "users/#{user_id}"
-      else
-        erb:"sessions/login"
-      end
-    end
+  #       redirect to "users/#{user_id}"
+  #     else
+  #       erb:"sessions/login"
+  #     end
+  #   end
 
 
-  post '/login' do
+  # post '/login' do
      
-      user = User.find_by(:email => params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "users/#{user.id}"
-      else
-        flash[:error] = "User not found, please Sign up First!"
-        redirect to '/users/new'
-      end
-    end
+  #     user = User.find_by(:email => params[:email])
+  #     if user && user.authenticate(params[:password])
+  #       session[:user_id] = user.id
+  #       redirect "users/#{user.id}"
+  #     else
+  #       flash[:error] = "User not found, please Sign up First!"
+  #       redirect to '/users/new'
+  #     end
+  #   end
 
-  get '/sessions/logout' do
-      if logged_in?
-        session.clear
-        redirect to '/'
-      else
-        redirect to '/'
-      end
-    end
+  
 end
