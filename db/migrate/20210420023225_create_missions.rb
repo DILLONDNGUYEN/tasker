7baaecc0599c79b253bcd97037f6b0900adfc8f9
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-04-19 19:36:13 -0700

    add mission

---
+++ db/migrate/20210420023225_create_missions.rb
@@ -0,0 +1,13 @@
+class CreateMissions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mission do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.has_many :tasks
+      t.datetime :created
+      t.text :title
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
