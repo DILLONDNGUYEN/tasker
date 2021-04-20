d44763985b2d2b47d560965f0123522b02fb4aa4
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-04-20 07:43:11 -0700

    add some user methods

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-#authentication stuff, might do omniauth
+#authentication stuff, might do omniauth 
 gem 'devise'
 gem 'dotenv-rails'
 gem 'omniauth'
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,24 @@
 class UserController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+       log_in(@user)
+      flash[:success] = "Account Created!"
+       else
+      render :new
+    end
+
+  end
+
+
 end
