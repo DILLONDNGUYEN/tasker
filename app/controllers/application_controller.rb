f871f9791419273d1aadfb5a00571d55ee0bcb53
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2021-04-19 22:13:17 -0700

    add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+      !!current_user
+    end
+    
+    def current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def authenticate
+      if !logged_in? || current_user.nil?
+        redirect '/login'
+      end
+    end
+
 end
--- db/migrate/20210420031249_create_tasks.rb
+++ db/migrate/20210420031249_create_tasks.rb
@@ -1,6 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
-    create_table :tasks do |t|
+    create_table :task do |t|
       t.belongs_to :mission, null: false, foreign_key: true
       t.text :description
       t.string :status
